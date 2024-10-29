8b488e4659bc2647dd1033e95ceb2f6735cc6271
John Duru <kachijohnduru@googlemail.com>
2024-10-29 00:57:19 +0000

    Removed unused variables

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Inter, Chakra_Petch, Saira } from "next/font/google";
+import { Inter} from "next/font/google";
 import "./globals.scss";
 
 const inter = Inter({ subsets: ["latin"] });
